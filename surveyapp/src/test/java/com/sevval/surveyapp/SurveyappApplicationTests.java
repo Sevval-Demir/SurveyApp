014627647686a3c704a3b3e97f3b6a9367812807
Sevval-Demir <sevvaldemirrt@gmail.com>
2025-07-21 16:39:19 +0300

    first commit

---
+++ surveyapp/src/test/java/com/sevval/surveyapp/SurveyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevval.surveyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
